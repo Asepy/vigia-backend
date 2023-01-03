f7294d27b1cc8f4973acf34cb680e4fbc764611a
bryantperez <marcelo.bmpr@gmail.com>
2023-01-03 11:09:31 -0600

    envs

--- handler.js
+++ handler.js
@@ -91,7 +91,7 @@ module.exports.testService = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!3",
+        message: "Go Serverless v1.0! Your function executed successfully!",
         bdTest:result,
         bdError:bdError,
         input: event
--- twitter_api/index.js
+++ twitter_api/index.js
@@ -5,8 +5,7 @@ const client = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY,
   appSecret: process.env.TWITTER_API_KEY_SECRET,
   accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  bearerToken: process.env.TWITTER_BEARER_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 async function createTweetOnAPI(tweetText) {
